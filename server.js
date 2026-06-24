beeb444249ba2463fd222cf000dc0774a8a5734c
mrwasif-dev <happyprincetemp@gmail.com>
2026-06-24 20:37:39 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+{
+  "name": "ludo-kingdom",
+  "version": "1.0.0",
+  "description": "Complete Ludo Game Platform with Authentication, Coins, and Multiplayer",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js",
+    "test": "echo \"No tests specified\" && exit 0"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5"
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.22"
+  },
+  "keywords": ["ludo", "game", "multiplayer", "board-game"],
+  "author": "Your Name",
+  "license": "MIT",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/yourusername/ludo-game.git"
+  }
+}
